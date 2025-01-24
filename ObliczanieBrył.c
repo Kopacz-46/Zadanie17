03aa5b98061bb30eb68abf8453b94e4be9a57457
Kacper Bartlomiejczak <kacper.bartlomiejczak@wp.pl>
2025-01-24 09:37:48 +0100

    Druga wersja. Obliczanie objętości kuli. ver0.0.2 (Zadanie17.1)

--- ObliczanieBrył.c
+++ ObliczanieBrył.c
@@ -5,20 +5,29 @@ return (a*b*c);
 
 }
 
+float sphere_volume (float r){
+return ((4.0/3.0)*3.14*(r*r*r));
+}
+
 int main () {
-    float a, b, c;
+    float a, b, c, r;
 
-    printf("podaj dlugosc podstawy twojego prostopadloscianu (a) \n");
-    scanf("%f", &a);
+    printf("podaj promien kuli (r) \n");
+    scanf ("%f", &r);
 
-    printf("podaj szerokosc podstawy twojego prostopadloscianu (b) \n");
-    scanf("%f", &b);
+    printf("objetosc twojej kuli wynosi: %f \n", sphere_volume(r));
 
-    printf("podaj wysokosc twojego prostopadloscianu (c) \n");
-    scanf("%f", &c);
+    //printf("podaj dlugosc podstawy twojego prostopadloscianu (a) \n");
+    //scanf("%f", &a);
 
-    printf("objetosc twojego prostopadloscianu wynosi: %f\n", cuboid_volume(a, b, c));
-    printf("\n Brawo obliczyles objetosc :)");
+    //printf("podaj szerokosc podstawy twojego prostopadloscianu (b) \n");
+    //scanf("%f", &b);
+
+    //printf("podaj wysokosc twojego prostopadloscianu (c) \n");
+    //scanf("%f", &c);
+
+    //printf("objetosc twojego prostopadloscianu wynosi: %f\n", cuboid_volume(a, b, c));
+    //printf("\n Brawo obliczyles objetosc :)");
 
 
 return(0);
